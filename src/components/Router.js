dc6555d8e54086cdcc7310df0ad3c4f3a64d8b1c
Samira Ahari <samira.ahari86@gmail.com>
2022-05-31 19:41:59 +0430

    Fix github pages issue

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,20 @@
-import React , {Component} from 'react';
-import {BrowserRouter , Switch , Route} from 'react-router-dom';
-import NotFound from './NotFound';
-import App from './App';
-import Shop from './Shop';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NotFound from "./NotFound";
+import App from "./App";
+import Shop from "./Shop";
 
-
-class Router extends Component{
-    render(){
-        return(
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path='/' component={Shop}/>
-                    <Route  path='/shop/:shopId' component={App}/>
-                    <Route  component={NotFound}/>
-                </Switch>
-            </BrowserRouter>
-        )
-    }
+class Router extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/shop" component={Shop} />
+          <Route path="/shop/:shopId" component={App} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 export default Router;
--- src/components/Toturial.js
+++ src/components/Toturial.js
@@ -1,33 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Toturial extends Component {
-    handeClick = ()=>{
-        this.props.addToOrder(this.props.index);
-    }
+  handeClick = () => {
+    this.props.addToOrder(this.props.index);
+  };
 
-    enter = (e) => {
-        e.currentTarget.innerHTML = `new Text`;
-        
-    }
+  enter = (e) => {
+    e.currentTarget.innerHTML = `new Text`;
+  };
 
-    leave = (e) => {
-        e.currentTarget.innerHTML = this.props.details.name;
-    }
+  leave = (e) => {
+    e.currentTarget.innerHTML = this.props.details.name;
+  };
 
-    render(){
-        
-        const {name,image,desc,price,status} = this.props.details;
-        const isAvailable = status === 'available';
-        return(
-            <div className='single-toturial'>
-                <img src={image}/>
-                <div className='details'>
-                    <h2 className='tutorial-name'><span onMouseEnter={this.enter} onMouseLeave={this.leave}>{name}</span><span className='price'>{price}</span></h2>
-                    <p>{desc}</p>
-                    <button onClick={this.handeClick} disabled={!isAvailable} className='add-to-order'>{isAvailable? 'Add To Order' : 'Sold Out' }</button>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { name, image, desc, price, status } = this.props.details;
+    const isAvailable = status === "available";
+    return (
+      <div className="single-toturial">
+        <img src={`/shop/${image}`} />
+        <div className="details">
+          <h2 className="tutorial-name">
+            <span onMouseEnter={this.enter} onMouseLeave={this.leave}>
+              {name}
+            </span>
+            <span className="price">{price}</span>
+          </h2>
+          <p>{desc}</p>
+          <button
+            onClick={this.handeClick}
+            disabled={!isAvailable}
+            className="add-to-order"
+          >
+            {isAvailable ? "Add To Order" : "Sold Out"}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 export default Toturial;
